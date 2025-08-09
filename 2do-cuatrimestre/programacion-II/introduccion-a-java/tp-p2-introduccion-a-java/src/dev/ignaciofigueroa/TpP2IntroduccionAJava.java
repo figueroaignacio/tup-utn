7518e81b85d4e3d108a5353ffe0234397bff404a
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-08-09 17:55:02 -0300

    agregar tp de programacion 2

---
+++ 2do-cuatrimestre/programacion-II/introduccion-a-java/tp-p2-introduccion-a-java/src/dev/ignaciofigueroa/TpP2IntroduccionAJava.java
@@ -0,0 +1,96 @@
+package dev.ignaciofigueroa;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ignacio Figueroa
+ * TP: Introducción a Java
+ * Comisión: 7
+ */
+
+public class TpP2IntroduccionAJava {
+    
+    public static void main(String[] args) {
+//        1.
+        
+//        String nombre = "Ignacio Figueroa";
+//        int edad = 21;
+//        double altura = 1.77;
+//        boolean estudiante = true;
+//        
+//        System.out.println("Hola, soy " + nombre + ", tengo " + edad + " y mido " + altura);
+        
+        
+//        2. Escribe un programa que solicite al usuario ingresar su nombre y edad, y luego los muestre en pantalla. 
+//        Usa Scanner para capturar los datos. 
+        
+//        Scanner input = new Scanner(System.in);
+//        String nombre;
+//        int edad;
+//        
+//        System.out.print("Ingrese su nombre: ");
+//        nombre = input.nextLine();
+//            
+//        System.out.print("Ingrese su edad: ");
+//        edad = Integer.parseInt( (input.nextLine()) );
+//        
+//        String resultado = "Hola, soy " + nombre + " y tengo " + edad;
+//                
+//        System.out.println(resultado);
+
+//        3. Escribe un programa que solicite dos números enteros y realice las siguientes operaciones:  
+//        a. Suma  b. Resta  c. Multiplicación  d. División  Muestra los resultados en la consola. 
+
+//          Scanner input = new Scanner(System.in);
+//          double x, y;
+//          
+//          System.out.print("Ingrese el primer número: ");
+//          x = Double.parseDouble( input.nextLine() );
+//          
+//          System.out.print("Ingrese el segundo número: ");
+//          y = Double.parseDouble( input.nextLine() );
+//          
+//          double suma = x + y;
+//          double resta = x - y;
+//          double multiplicacion = x * y;
+//          double division = x / y;
+//          
+//          System.out.println("Suma: " + suma);
+//          System.out.println("Resta: " + resta);
+//          System.out.println("Multiplicación: " + multiplicacion);
+//          System.out.println("División: " + division);
+
+//          4. Escribe un programa que muestre el siguiente mensaje en consola: 
+//          
+//          Nombre: Juan Pérez  
+//          Edad: 30 años  
+//          Dirección: "Calle Falsa 123"  
+//          
+//          Usa caracteres de escape (\n, \") en System.out.println(). 
+          
+//          String nombre = "Ignacio Figueroa";
+//          int edad = 21;
+//          String direccion = "Ramón Santamarina 1081, Monte Grande, Provincia de Buenos Aires.";
+//            
+//          System.out.println("Nombre: " + nombre + "\n" + "Edad: " + edad + "\n" + "Dirección: " + direccion);
+
+//          5. Corrige el siguiente código para que funcione correctamente. Explica qué error 
+//          tenía y cómo lo solucionaste. 
+
+//          CODIGO CON ERROR
+
+//          Scanner scanner = new Scanner(System.in); 
+//          System.out.print("Ingresa tu nombre: "); 
+//          String nombre = scanner.nextInt(); // ERROR 
+//          System.out.println("Hola, " + nombre); 
+
+//          CODIGO CORREGIDO
+
+//          Scanner scanner = new Scanner(System.in);
+//          System.out.print("Ingresa tu nombre: ");
+//          String nombre = scanner.nextLine(); 
+//          System.out.println("Hola, " + nombre);
+    }
+    
+}
